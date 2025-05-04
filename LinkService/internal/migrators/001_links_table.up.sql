5b3773664ddd0e33934d272302fe0ffe9ab91528
MishaZhem <mz-100@mail.ru>
2025-05-04 14:30:49 +0200

    chore: change table

--- LinkService/internal/migrators/001_links_table.up.sql
+++ LinkService/internal/migrators/001_links_table.up.sql
@@ -1,6 +1,6 @@
--- migrate -path internal/migrators -database "postgres://postgres:123@localhost:5432/postgres?sslmode=disable" up
+-- migrate -path internal/migrators -database "postgres://postgres:123@localhost:5433/postgres?sslmode=disable" up
 CREATE TABLE IF NOT EXISTS links (
     id serial not null,
-    key VARCHAR(25) not null,
-    original_url VARCHAR(25) not null
+    key TEXT not null,
+    original_url TEXT not null
 );
